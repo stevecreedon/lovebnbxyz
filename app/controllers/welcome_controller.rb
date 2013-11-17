cd29d58390845473cf315811421ff6c7330a8db0
stevecreedon <steve@creedon.me>
2013-11-17 17:34:03 +0000

    testing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 
+  layout "application", only: [:show]
+
   def index
+    render "welcome/index", :layout => false
+  end
+
+  def show
     @site = Site.first#Site.where(domain: request.domain, subdomain: request.subdomain).first
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,8 @@ Lovebnbwww::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  
+  match 'sites/:theme' => 'welcome#show'
   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
